84c97249389c2ddc65e6b562f4a6f30fc113ccb9
gitTaynara4x <git.taynara.4x4@gmail.com>
2025-02-24 16:35:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -241,4 +241,4 @@ def atualizar_cidade_uf(deal_id, cep):
 
 #rua, numero, bairro, cidade - estado, cep
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=35)
+    app.run(host='0.0.0.0', port=1474)
